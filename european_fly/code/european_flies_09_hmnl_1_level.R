3b60f255c30c3e95a9f22caba40d1b00ecd138af
Mario Becerra <mbecerracontreras@gmail.com>
2023-05-26 21:19:59 +0200

    1 level model with no covariates and different parametrizations to test

---
+++ european_fly/code/european_flies_09_hmnl_1_level.R
@@ -0,0 +1,576 @@
+library(tidyverse)
+library(rstan)
+library(here)
+
+# To stop Stan files from crashing RStudio
+rstan_options(javascript=FALSE)
+
+source(here("european_fly/code/european_flies_09_hmnl_1_level_utils.R"))
+
+counts_european = read_csv(here("european_fly/out/counts_european_choice_format.csv")) %>% 
+  filter(experiment != 1) %>% 
+  mutate(experiment = experiment - 1)
+
+# There are some choice sets with only one image. Check that.
+n_images_per_cs = counts_european %>% 
+  group_by(experiment, folder, choice_set) %>% 
+  summarize(n_images = max(image))
+
+# Take subset of images
+set.seed(2023)
+images_indices = counts_european %>%
+  left_join(n_images_per_cs) %>% 
+  filter(n_images > 1) %>% 
+  select(experiment, folder, choice_set, image) %>% 
+  distinct() %>% 
+  group_by(experiment, folder, choice_set) %>% 
+  slice_sample(n = 10) %>% 
+  arrange(experiment, choice_set, image) %>% 
+  ungroup()
+
+
+counts_european_sample <- images_indices %>% 
+  left_join(counts_european) %>% 
+  as.data.frame() %>% 
+  mutate(time_day = as.numeric(substr(as.character(date_time), 12, 13))) %>% 
+  mutate(
+    is_right = ifelse(alternative == "2_right", 1, 0),
+    is_daytime = ifelse(time_day >= 9 & time_day <= 20, 1, 0) # 1 = 9-20h, 0 = 21-23h + 0-8h
+  ) 
+
+
+# Stan data ------------
+unique_experiment_choice_set = counts_european_sample %>%
+  select(experiment, folder) %>%
+  distinct()
+
+
+index_dataframe = lapply(1:nrow(unique_experiment_choice_set), function(i){
+  exp_i = unique_experiment_choice_set$experiment[i]
+  cs_i =  unique_experiment_choice_set$folder[i]
+  
+  indices_i = which(counts_european_sample$folder == cs_i & counts_european_sample$experiment == exp_i)
+  out = tibble(
+    choice_set = cs_i,
+    start = indices_i[1],
+    end = indices_i[length(indices_i)],
+    exp = exp_i
+  )
+}) %>%
+  bind_rows()
+
+
+unique_experiment_choice_set_image = counts_european_sample %>%
+  select(experiment, folder, image) %>%
+  distinct()
+
+index_dataframe_image = lapply(1:nrow(unique_experiment_choice_set_image), function(i){
+  exp_i = unique_experiment_choice_set_image$experiment[i]
+  cs_i =  unique_experiment_choice_set_image$folder[i]
+  image_i =  unique_experiment_choice_set_image$image[i]
+  
+  indices_i = which(counts_european_sample$folder == cs_i & counts_european_sample$experiment == exp_i & counts_european_sample$image == image_i)
+  out = tibble(
+    exp = exp_i,
+    choice_set = cs_i,
+    image = image_i,
+    start = indices_i[1],
+    end = indices_i[length(indices_i)]
+  )
+}) %>%
+  bind_rows() %>% 
+  mutate(choice_set_2 = as.integer(fct(paste(exp, choice_set))))
+
+
+
+df_to_fit <- counts_european_sample %>% 
+  select(all_of(c('R','O','Y','G','B','P','UV','intensity','is_right', 'no_choice'))) %>%
+  as.data.frame()
+
+
+X_stan_list = create_model_matrix_second_order_scheffe(df_to_fit)
+
+
+
+n_experiments = max(counts_european_sample$experiment)
+n_mixture_cols = dim(X_stan_list$X)[2] - 1
+
+
+
+stan_data <- list(
+  n_alt = 3, 
+  n_exp = n_experiments,
+  n_var = ncol(X_stan_list$X), 
+  n_obs = nrow(X_stan_list$X),
+  Ny = counts_european_sample$count,
+  X = X_stan_list$X,
+  experiment = index_dataframe$exp,
+  choice_set = index_dataframe$choice_set,
+  
+  start = index_dataframe_image$start, # the starting observation for each image
+  end = index_dataframe_image$end,  # the ending observation for each image
+  n_images = nrow(index_dataframe_image),
+  exp_index = index_dataframe_image$exp,
+  
+  n_mixture_cols = n_mixture_cols
+)
+
+
+
+# Stan model ------------
+
+init_fun <- function() {
+  out_list = list(
+    # tau_level_0 = rep(1, n_mixture_cols+1), 
+    # Omega_level_0 = diag(1, ncol = n_mixture_cols+1, nrow = n_mixture_cols+1)
+    # tau_level_0 = rnorm(n_mixture_cols+1, mean = 1, sd = 0.1), 
+    tau_level_0 = rgamma(n_mixture_cols+1, shape = 4, scale = 0.25),
+    Omega_level_0 = rlkjcorr(K = n_mixture_cols+1, eta = 30),
+    L_Omega_level_0 = chol(rlkjcorr(K = n_mixture_cols+1, eta = 30))
+  )
+  
+  return(out_list)
+}
+
+
+# 1158 seconds with 100 iterations
+# 1000 transitions using 10 leapfrog steps per transition would take 176 to 210 seconds
+model_stan_01 <- stan(
+  file = here("european_fly/code/european_flies_09_hmnl_1_level_01.stan"),
+  data = stan_data,
+  seed = 2023,
+  # iter = 1500,  warmup = 1000, chains = 4, cores = 4,
+  # iter = 2500,  warmup = 2000, chains = 4, cores = 4,
+  # iter = 3000,  warmup = 2000, chains = 6, cores = 6,
+  iter = 100, chains = 4, cores = 4,
+  # iter = 25, chains = 1,
+  init = init_fun
+)
+
+model_stan_01
+
+
+
+
+# 1153 seconds with 100 iterations
+# 1000 transitions using 10 leapfrog steps per transition would take 176 to 210 seconds
+model_stan_02 <- stan(
+  file = here("european_fly/code/european_flies_09_hmnl_1_level_02.stan"),
+  data = stan_data,
+  seed = 2023,
+  # iter = 1500,  warmup = 1000, chains = 4, cores = 4,
+  # iter = 2500,  warmup = 2000, chains = 4, cores = 4,
+  # iter = 3000,  warmup = 2000, chains = 6, cores = 6,
+  iter = 100,  chains = 4, cores = 4,
+  # iter = 25, chains = 1,
+  init = init_fun
+)
+
+model_stan_02
+
+
+
+
+
+
+
+# 1153 seconds with 100 iterations
+# Gradient evaluation took 0.0209 seconds
+# 1000 transitions using 10 leapfrog steps per transition would take 209 to 220 seconds
+model_stan_02 <- stan(
+  file = here("european_fly/code/european_flies_09_hmnl_1_level_02.stan"),
+  data = stan_data,
+  seed = 2023,
+  # iter = 1500,  warmup = 1000, chains = 4, cores = 4,
+  # iter = 2500,  warmup = 2000, chains = 4, cores = 4,
+  # iter = 3000,  warmup = 2000, chains = 6, cores = 6,
+  iter = 100,  chains = 4, cores = 4,
+  # iter = 25, chains = 1,
+  init = init_fun
+)
+
+model_stan_02
+
+
+
+
+
+
+#  seconds with 50 iterations
+# Gradient evaluation took 0.0181 seconds
+# 1000 transitions using 10 leapfrog steps per transition would take 181 to 185 seconds
+model_stan_03 <- stan(
+  file = here("european_fly/code/european_flies_09_hmnl_1_level_03.stan"),
+  data = stan_data,
+  seed = 2023,
+  # iter = 1500,  warmup = 1000, chains = 4, cores = 4,
+  # iter = 2500,  warmup = 2000, chains = 4, cores = 4,
+  # iter = 3000,  warmup = 2000, chains = 6, cores = 6,
+  # iter = 100,  chains = 4, cores = 4,
+  # iter = 25, chains = 1,
+  iter = 50, chains = 4, cores = 4,
+  init = init_fun
+)
+
+model_stan_03
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+summary(model_stan_01, pars = c("alpha"), probs = c(0.1, 0.5, 0.9))$summary
+summary(model_stan_01, pars = c("beta_level_0"), probs = c(0.1, 0.5, 0.9))$summary
+summary(model_stan_01, pars = c("beta_level_1"), probs = c(0.1, 0.5, 0.9))$summary
+
+
+
+plot(model_stan_01, plotfun="trace", pars=("alpha"))
+plot(model_stan_01, plotfun="trace", pars=("beta_level_0"))
+plot(model_stan_01, plotfun="trace", pars=("beta_level_1"))
+
+
+
+prior_params_summary = tibble(prior_mean = c(-2.99, -1.02, -0.25, -0.63, -2.54, -2.19, 5.41, 8.31, 2.64, 1.13, 15.71, 9.36, 3.25, 2.00, 3.65, 5.79, 2.75, -0.68, 7.72, -6.42, -4.48, 11.68, 8.30, 3.36, -7.40, 9.02, 6.10, 1.14, 3.09, 1.70, -0.21, -0.93, -0.32, -0.79, -0.04, 4.79, 0)) %>%
+  mutate(prior_sd = 2)
+
+
+betas_level_0_summary_01 = summary(model_stan_01, pars = c("beta_level_0", "alpha"), probs = c(0.025, 0.1, 0.5, 0.9, 0.975))$summary %>% 
+  as.data.frame() %>% 
+  select("mean", "sd", "2.5%", "10%", "90%", "97.5%") %>% 
+  mutate(variable = colnames(X_stan_list$X),
+         ix = 1:n()) %>%
+  mutate(variable = fct_reorder(variable, ix, .desc = T)) %>% 
+  bind_cols(prior_params_summary)
+
+betas_level_0_summary_01 %>% 
+  ggplot(aes(x = variable)) +
+  geom_point(aes(y = mean), color = "black") +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
+  coord_flip() +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value")
+
+
+
+betas_level_0_summary_01 %>% 
+  mutate(variable = fct_reorder(variable, ix, .desc = F)) %>% 
+  ggplot(aes(x = variable)) +
+  geom_point(aes(y = mean), color = "black") +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+
+
+betas_level_0_summary_01 %>% 
+  mutate(variable = fct_reorder(variable, ix, .desc = F)) %>% 
+  select(variable, mean, sd) %>% 
+  mutate(dist = "posterior") %>% 
+  bind_rows(
+    betas_level_0_summary_01 %>% 
+      select(variable, mean = prior_mean, sd = prior_sd) %>% 
+      mutate(dist = "prior")
+  ) %>% 
+  ggplot(aes(x = variable, color = dist)) +
+  geom_hline(yintercept = 0, size = 0.3) +
+  geom_point(aes(y = mean), position = position_dodge(width = 0.5)) +
+  geom_linerange(aes(ymin = mean - sd, ymax = mean + sd), size = 0.9, position = position_dodge(width = 0.5)) +
+  geom_linerange(aes(ymin = mean - 2*sd, ymax = mean + 2*sd), size = 0.5, position = position_dodge(width = 0.5)) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  ggtitle("Beta level 0", subtitle = "Prior and posterior") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+
+
+
+names_betas_level_1 = X_stan_list$names_betas_level_1
+
+
+betas_level_1_summary = summary(model_stan_01, pars = c("beta_level_1"), probs = c(0.025, 0.1, 0.5, 0.9, 0.975))$summary %>% 
+  as.data.frame() %>% 
+  select("mean", "2.5%", "10%", "90%", "97.5%") %>% 
+  mutate(
+    exp = paste0(
+      "Exp ",
+      rep(1:n_experiments, each = length(names_betas_level_1))
+    ),
+    variable = rep(names_betas_level_1, n_experiments)
+  ) %>% 
+  group_by(exp) %>% 
+  mutate(ix = 1:n()) %>%
+  ungroup() %>% 
+  # mutate(variable = paste0(exp, ", ", var)) %>% 
+  mutate(variable = fct_reorder(variable, ix, .desc = T)) 
+
+
+
+betas_level_1_summary %>% 
+  ggplot(aes(x = variable)) +
+  geom_point(aes(y = mean), color = "black") +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
+  facet_wrap(~exp) +
+  coord_flip() +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value")
+
+
+
+betas_level_1_summary %>% 
+  mutate(variable = fct_reorder(variable, ix, .desc = F)) %>% 
+  ggplot(aes(x = variable, color = exp)) +
+  geom_hline(yintercept = 0, size = 0.3) +
+  geom_point(aes(y = mean), position = position_dodge(width = 0.6), size = 0.5) +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), size = 0.3, position = position_dodge(width = 0.6)) +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), size = 0.7, position = position_dodge(width = 0.6)) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  geom_vline(xintercept = 1:36 + 0.5, size = 0.3, color = "black") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank())
+
+
+
+Sigma_level_0_posterior_median = matrix(as.data.frame(summary(model_stan_01, pars = c("Sigma_level_0"), probs = c(0.5))$summary)$`50%`, 
+                                        ncol = stan_data$n_mixture_cols+1)
+
+# Sigma_level_1_posterior_median = matrix(as.data.frame(summary(model_stan_01, pars = c("Sigma_level_1"), probs = c(0.5))$summary)$`50%`, 
+#                                         ncol = stan_data$n_mixture_cols+1)
+
+
+diag(Sigma_level_0_posterior_median)
+# diag(Sigma_level_1_posterior_median)
+
+
+
+utilities_all_mean = get_posterior_mean(model_stan_01, "utilities_all")
+
+counts_european_sample %>% 
+  mutate(utility_model = utilities_all_mean[, ncol(utilities_all_mean)]) %>% 
+  filter(no_choice == 0) %>% 
+  select(all_of(names(df_to_fit)), utility_model) %>% 
+  distinct() %>% 
+  group_by(across(all_of(c('no_choice','is_right')))) %>% 
+  top_n(3, utility_model) %>% 
+  as.data.frame() %>% 
+  ungroup() %>% 
+  arrange(desc(utility_model))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+summary(model_stan_02, pars = c("alpha"), probs = c(0.1, 0.5, 0.9))$summary
+summary(model_stan_02, pars = c("beta_level_0"), probs = c(0.1, 0.5, 0.9))$summary
+summary(model_stan_02, pars = c("beta_level_1"), probs = c(0.1, 0.5, 0.9))$summary
+
+
+
+plot(model_stan_02, plotfun="trace", pars=("alpha"))
+plot(model_stan_02, plotfun="trace", pars=("beta_level_0"))
+plot(model_stan_02, plotfun="trace", pars=("beta_level_1"))
+
+
+
+prior_params_summary = tibble(prior_mean = c(-2.99, -1.02, -0.25, -0.63, -2.54, -2.19, 5.41, 8.31, 2.64, 1.13, 15.71, 9.36, 3.25, 2.00, 3.65, 5.79, 2.75, -0.68, 7.72, -6.42, -4.48, 11.68, 8.30, 3.36, -7.40, 9.02, 6.10, 1.14, 3.09, 1.70, -0.21, -0.93, -0.32, -0.79, -0.04, 4.79, 0)) %>%
+  mutate(prior_sd = 2)
+
+
+betas_level_0_summary_02 = summary(model_stan_02, pars = c("beta_level_0", "alpha"), probs = c(0.025, 0.1, 0.5, 0.9, 0.975))$summary %>% 
+  as.data.frame() %>% 
+  select("mean", "sd", "2.5%", "10%", "90%", "97.5%") %>% 
+  mutate(variable = colnames(X_stan_list$X),
+         ix = 1:n()) %>%
+  mutate(variable = fct_reorder(variable, ix, .desc = T)) %>% 
+  bind_cols(prior_params_summary)
+
+betas_level_0_summary_02 %>% 
+  ggplot(aes(x = variable)) +
+  geom_point(aes(y = mean), color = "black") +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
+  coord_flip() +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value")
+
+
+
+betas_level_0_summary_02 %>% 
+  mutate(variable = fct_reorder(variable, ix, .desc = F)) %>% 
+  ggplot(aes(x = variable)) +
+  geom_point(aes(y = mean), color = "black") +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+
+
+
+betas_level_0_summary_02 %>% 
+  mutate(variable = fct_reorder(variable, ix, .desc = F)) %>% 
+  select(variable, mean, sd) %>% 
+  mutate(dist = "posterior") %>% 
+  bind_rows(
+    betas_level_0_summary_02 %>% 
+      select(variable, mean = prior_mean, sd = prior_sd) %>% 
+      mutate(dist = "prior")
+  ) %>% 
+  ggplot(aes(x = variable, color = dist)) +
+  geom_hline(yintercept = 0, size = 0.3) +
+  geom_point(aes(y = mean), position = position_dodge(width = 0.5)) +
+  geom_linerange(aes(ymin = mean - sd, ymax = mean + sd), size = 0.9, position = position_dodge(width = 0.5)) +
+  geom_linerange(aes(ymin = mean - 2*sd, ymax = mean + 2*sd), size = 0.5, position = position_dodge(width = 0.5)) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  ggtitle("Beta level 0", subtitle = "Prior and posterior") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+
+
+
+names_betas_level_1 = X_stan_list$names_betas_level_1
+
+
+betas_level_1_summary = summary(model_stan_02, pars = c("beta_level_1"), probs = c(0.025, 0.1, 0.5, 0.9, 0.975))$summary %>% 
+  as.data.frame() %>% 
+  select("mean", "2.5%", "10%", "90%", "97.5%") %>% 
+  mutate(
+    exp = paste0(
+      "Exp ",
+      rep(1:n_experiments, each = length(names_betas_level_1))
+    ),
+    variable = rep(names_betas_level_1, n_experiments)
+  ) %>% 
+  group_by(exp) %>% 
+  mutate(ix = 1:n()) %>%
+  ungroup() %>% 
+  # mutate(variable = paste0(exp, ", ", var)) %>% 
+  mutate(variable = fct_reorder(variable, ix, .desc = T)) 
+
+
+
+betas_level_1_summary %>% 
+  ggplot(aes(x = variable)) +
+  geom_point(aes(y = mean), color = "black") +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
+  facet_wrap(~exp) +
+  coord_flip() +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value")
+
+
+
+betas_level_1_summary %>% 
+  mutate(variable = fct_reorder(variable, ix, .desc = F)) %>% 
+  ggplot(aes(x = variable, color = exp)) +
+  geom_hline(yintercept = 0, size = 0.3) +
+  geom_point(aes(y = mean), position = position_dodge(width = 0.6), size = 0.5) +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), size = 0.3, position = position_dodge(width = 0.6)) +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), size = 0.7, position = position_dodge(width = 0.6)) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  geom_vline(xintercept = 1:36 + 0.5, size = 0.3, color = "black") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank())
+
+
+
+Sigma_level_0_posterior_median = matrix(as.data.frame(summary(model_stan_02, pars = c("Sigma_level_0"), probs = c(0.5))$summary)$`50%`, 
+                                        ncol = stan_data$n_mixture_cols+1)
+
+# Sigma_level_1_posterior_median = matrix(as.data.frame(summary(model_stan_02, pars = c("Sigma_level_1"), probs = c(0.5))$summary)$`50%`, 
+#                                         ncol = stan_data$n_mixture_cols+1)
+
+
+diag(Sigma_level_0_posterior_median)
+# diag(Sigma_level_1_posterior_median)
+
+
+
+utilities_all_mean = get_posterior_mean(model_stan_02, "utilities_all")
+
+counts_european_sample %>% 
+  mutate(utility_model = utilities_all_mean[, ncol(utilities_all_mean)]) %>% 
+  filter(no_choice == 0) %>% 
+  select(all_of(names(df_to_fit)), utility_model) %>% 
+  distinct() %>% 
+  group_by(across(all_of(c('no_choice','is_right')))) %>% 
+  top_n(3, utility_model) %>% 
+  as.data.frame() %>% 
+  ungroup() %>% 
+  arrange(desc(utility_model))
---
+++ european_fly/code/european_flies_09_hmnl_1_level_02.stan
@@ -0,0 +1,66 @@
+data {
+  int<lower=2> n_alt; // number of alternatives/outcomes
+  int<lower=1> n_var; // of covariates
+  int<lower=1> n_obs; // of observations
+  int<lower=1> n_exp; // Number of experiments
+  vector[n_obs] Ny; // counts of the response variable
+  matrix[n_obs, n_var] X; // attribute matrix
+  int n_images;
+  int start[n_images]; // the starting observation for each image
+  int end[n_images]; // the ending observation for each image
+  int exp_index[n_images];
+
+}
+
+parameters {
+  vector[n_var] beta_level_0;
+  vector[n_var] beta_level_1[n_exp];
+  // corr_matrix[n_var] Omega_level_0;
+  cholesky_factor_corr[n_var] L_Omega_level_0;
+  vector<lower=0>[n_var] tau_level_0;
+}
+
+transformed parameters {
+  matrix[n_var, n_var] Sigma_level_0 = diag_pre_multiply(tau_level_0, L_Omega_level_0) * diag_pre_multiply(tau_level_0, L_Omega_level_0)';
+}
+
+model {
+  vector[n_alt] utilities;
+
+  
+  // In LKJ:
+  // if eta = 1, then the density is uniform over correlation matrices of a given order K (the number of row/column).
+  // if eta > 1, the correlation values in correlation matrices are going to centered around 0. higher eta indicate no correlations (converge to identity correlation matrix).
+  // https://yingqijing.medium.com/lkj-correlation-distribution-in-stan-29927b69e9be
+  
+  tau_level_0 ~ normal(1, 0.5); 
+  // Omega_level_0 ~ lkj_corr(5);
+  L_Omega_level_0 ~ lkj_corr_cholesky(5);
+  
+  beta_level_0 ~ normal(0, 2);
+  
+  
+  for(i in 1:n_exp){
+    beta_level_1[i] ~ multi_normal(beta_level_0, Sigma_level_0);
+    // beta_level_1[i] ~ multi_normal(beta_level_0, diag_pre_multiply(tau_level_0, L_Omega_level_0));
+  }
+
+  for(i in 1:n_images){
+    utilities = X[start[i]:end[i], 1:n_var]*beta_level_1[exp_index[i]];
+    target += sum(log_softmax(utilities) .* Ny[start[i]:end[i]]);
+  }
+  
+  
+}
+
+ 
+// // Save log-likelihood for LOO package
+// generated quantities{
+//   vector[n_images] log_lik;
+// 
+//   for(i in 1:n_images){
+//       log_lik[i] = sum(log_softmax(X[start[i]:end[i]]*beta_level_1[exp_index[i]]) .* Ny[start[i]:end[i]]);
+//     }
+// }
+// 
+
