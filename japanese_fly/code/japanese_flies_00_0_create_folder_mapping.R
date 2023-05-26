2af58f9b839edce55248dcc28bec51ecc0d65f92
Mario Becerra <mbecerracontreras@gmail.com>
2023-05-26 19:56:29 +0200

    Update design mapping code with 3rd design

--- japanese_fly/code/japanese_flies_00_0_create_folder_mapping.R
+++ japanese_fly/code/japanese_flies_00_0_create_folder_mapping.R
@@ -5,10 +5,13 @@ design_1_mapping = read_csv(here("data/design_1_mapping.csv"))
 
 design_2_mapping = read_csv(here("out/2nd_i_optimal_design_japanese_mapping.csv"))
 
+design_3_mapping = read_csv(here("out/3rd_i_optimal_design_japanese_mapping.csv"))
 
-design_mapping = design_1_mapping %>% 
+
+design_mapping = design_1_mapping %>%
   bind_rows(design_2_mapping)
-
+bind_rows(design_2_mapping) %>%
+  bind_rows(design_3_mapping)
 
 write_csv(design_mapping, "out/design_mapping_japanese.csv")
 
