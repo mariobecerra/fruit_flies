78387a476bfca0dcb6aa267dd1080a1aa888d59b
Mario Becerra <mbecerracontreras@gmail.com>
2023-06-01 15:35:59 +0200

    Modify figures and filenames of model

--- japanese_fly/code/japanese_flies_hmnl_1_level.R
+++ japanese_fly/code/japanese_flies_hmnl_1_level.R
@@ -28,13 +28,14 @@ n_images_per_cs = counts_japanese_filtered %>%
   summarize(n_images = length(unique(image)))
 
 
+n_images_per_cs_to_sample = 30
 # Take subset of images
 set.seed(2023)
 images_indices = counts_japanese_filtered %>%
   select(experiment, folder, choice_set, image) %>% 
   distinct() %>% 
   group_by(experiment, folder, choice_set) %>% 
-  slice_sample(n = 30) %>% 
+  slice_sample(n = n_images_per_cs_to_sample) %>% 
   arrange(experiment, choice_set, image) %>% 
   ungroup()
 
@@ -47,6 +48,10 @@ counts_japanese_sample <- images_indices %>%
     is_right = ifelse(alternative == "2_right", 1, 0),
     is_daytime = ifelse(time_day >= 9 & time_day <= 20, 1, 0) # 1 = 9-20h, 0 = 21-23h + 0-8h
   ) 
+
+model_01_dataframe_filename = here(paste0("japanese_fly/out/japanese_hmnl_model_experiments_1_to_", n_experiments, "_dataframe_", n_images_per_cs_to_sample, "images.rds"))
+
+saveRDS(counts_japanese_sample, model_01_dataframe_filename)
 
 
 # Stan data ------------
@@ -154,20 +159,39 @@ init_fun <- function() {
 # Gradient evaluation took 0.035647 seconds
 # 1000 transitions using 10 leapfrog steps per transition would take 356.47 seconds.
 # 17 minutes for 50 iterations
+#
+# 3 hours with 200 iterations
+# There were 400 transitions after warmup that exceeded the maximum treedepth. Increase max_treedepth above 10. See http://mc-stan.org/misc/warnings.html#maximum-treedepth-exceeded
+# The largest R-hat is 1.21, indicating chains have not mixed.
+# Running the chains for more iterations may help
+#
+# 4.5 hours with 500 iterations (350 warmup)
+# There were 179 transitions after warmup that exceeded the maximum treedepth. Increase max_treedepth above 10
 
-model_stan_01 <- stan(
-  file = here("japanese_fly/code/japanese_flies_hmnl_1_level_reparameterized.stan"),
-  data = stan_data,
-  seed = 2023,
-  include = F, pars = c("z", "L_Omega_level_0", "tau_level_0"),
-  # iter = 1500,  warmup = 1000, chains = 4, cores = 4,
-  # iter = 2500,  warmup = 2000, chains = 4, cores = 4,
-  # iter = 3000,  warmup = 2000, chains = 6, cores = 6,
-  # iter = 500,  chains = 4, cores = 4,
-  iter = 5, chains = 1,
-  # iter = 50, chains = 4, cores = 4,
-  init = init_fun
-)
+model_01_filename = here(paste0("japanese_fly/out/japanese_hmnl_model_experiments_1_to_", n_experiments, "_stan_object_", n_images_per_cs_to_sample, "images.rds"))
+
+if(file.exists(model_01_filename)){
+  cat("File already exists")
+  model_stan_01 = readRDS(model_01_filename)
+} else{
+  Sys.time()
+  model_stan_01 <- stan(
+    file = here("japanese_fly/code/japanese_flies_hmnl_1_level_reparameterized.stan"),
+    data = stan_data,
+    seed = 2023,
+    include = F, pars = c("z", "L_Omega_level_0", "tau_level_0"),
+    # iter = 1500,  warmup = 1000, chains = 4, cores = 4,
+    # iter = 2500,  warmup = 2000, chains = 4, cores = 4,
+    # iter = 3000,  warmup = 2000, chains = 6, cores = 6,
+    # iter = 500,  chains = 4, cores = 4,
+    # iter = 5, chains = 1,
+    # iter = 50, chains = 4, cores = 4,
+    iter = 500,  warmup = 350, chains = 4, cores = 4,
+    init = init_fun
+  )
+  Sys.time()
+  saveRDS(model_stan_01, model_01_filename)
+}
 
 model_stan_01
 
@@ -190,47 +214,18 @@ betas_level_0_summary_01 = summary(model_stan_01, pars = c("beta_level_0"), prob
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-betas_level_0_summary_01 %>% 
-  ggplot(aes(x = variable)) +
-  geom_point(aes(y = mean), color = "black") +
-  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
-  geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
-  coord_flip() +
-  theme_bw() +
-  xlab("Parameter") +
-  ylab("Value")
-
-
-
 betas_level_0_summary_01 %>% 
   mutate(variable = fct_reorder(variable, ix, .desc = F)) %>% 
   ggplot(aes(x = variable)) +
+  geom_hline(yintercept = 0) +
   geom_point(aes(y = mean), color = "black") +
   geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
   geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
   theme_bw() +
   xlab("Parameter") +
   ylab("Value") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  ggtitle(paste0("Japanese fly hyper betas, experiments 1 to ", n_experiments))
 
 
 
@@ -240,7 +235,7 @@ names_betas_level_1 = X_stan_list$names_betas_level_1
 
 betas_level_1_summary_01 = summary(model_stan_01, pars = c("beta_level_1"), probs = c(0.025, 0.1, 0.5, 0.9, 0.975))$summary %>% 
   as.data.frame() %>% 
-  select("mean", "2.5%", "10%", "90%", "97.5%") %>% 
+  select("mean","sd", "2.5%", "10%", "90%", "97.5%") %>% 
   mutate(
     exp = paste0(
       "Exp ",
@@ -258,36 +253,107 @@ betas_level_1_summary_01 = summary(model_stan_01, pars = c("beta_level_1"), prob
 
 betas_level_1_summary_01 %>% 
   ggplot(aes(x = variable)) +
+  geom_hline(yintercept = 0) +
   geom_point(aes(y = mean), color = "black") +
   geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), color = "dark grey") +
   geom_linerange(aes(ymin = `10%`, ymax = `90%`), color = "black", size = 1) +
   facet_wrap(~exp) +
-  coord_flip() +
   theme_bw() +
   xlab("Parameter") +
-  ylab("Value")
+  ylab("Value") +
+  ggtitle(paste0("Japanese fly betas of experiments 1 to ", n_experiments)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 
 
 
 betas_level_1_summary_01 %>% 
-  mutate(variable = fct_reorder(variable, ix, .desc = F)) %>% 
   ggplot(aes(x = variable, color = exp)) +
+  geom_hline(yintercept = 0) +
   geom_hline(yintercept = 0, size = 0.3) +
-  geom_point(aes(y = mean), position = position_dodge(width = 0.6), size = 0.5) +
-  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), size = 0.3, position = position_dodge(width = 0.6)) +
-  geom_linerange(aes(ymin = `10%`, ymax = `90%`), size = 0.7, position = position_dodge(width = 0.6)) +
+  geom_point(aes(y = mean), position = position_dodge(width = 0.6), size = 0.9) +
+  geom_linerange(aes(ymin = `2.5%`, ymax = `97.5%`), size = 0.8, position = position_dodge(width = 0.6)) +
+  geom_linerange(aes(ymin = `10%`, ymax = `90%`), size = 1.2, position = position_dodge(width = 0.6)) +
   theme_bw() +
   xlab("Parameter") +
   ylab("Value") +
   geom_vline(xintercept = 1:36 + 0.5, size = 0.3, color = "black") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
         panel.grid.major.x = element_blank(),
-        panel.grid.minor.x = element_blank())
+        panel.grid.minor.x = element_blank()) +
+  ggtitle(paste0("Japanese fly betas of experiments 1 to ", n_experiments))
+
+
+betas_level_1_summary_01 %>% 
+  ggplot(aes(x = variable, color = exp)) +
+  geom_hline(yintercept = 0) +
+  geom_hline(yintercept = 0, size = 0.3) +
+  geom_point(aes(y = mean), position = position_dodge(width = 0.6), size = 0.9) +
+  geom_linerange(aes(ymin = mean - 2*sd, ymax = mean + 2*sd), size = 0.8, position = position_dodge(width = 0.6)) +
+  geom_linerange(aes(ymin = mean - sd, ymax = mean + sd), size = 1.2, position = position_dodge(width = 0.6)) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  geom_vline(xintercept = 1:36 + 0.5, size = 0.3, color = "black") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) +
+  ggtitle(paste0("Japanese fly betas of experiments 1 to ", n_experiments))
 
 
 
-Sigma_level_0_posterior_median_01 = matrix(as.data.frame(summary(model_stan_02, pars = c("Sigma_level_0"), probs = c(0.5))$summary)$`50%`, 
-                                           ncol = stan_data$n_mixture_cols+1)
+betas_level_0_summary_01 %>% 
+  mutate(exp = "All experiments", line_thickness_1 = 2, line_thickness_2 = 1) %>% 
+  bind_rows(betas_level_1_summary_01 %>% 
+              mutate(line_thickness_1 = 0.6, line_thickness_2 = 0.4)) %>% 
+  ggplot(aes(x = variable, group = exp)) +
+  geom_hline(yintercept = 0) +
+  geom_hline(yintercept = 0, size = 0.3) +
+  geom_point(aes(y = mean), position = position_dodge(width = 0.7), size = 0.9) +
+  geom_linerange(aes(ymin = mean - 2*sd, ymax = mean + 2*sd, size = line_thickness_2), 
+                 position = position_dodge(width = 0.7)) +
+  geom_linerange(aes(ymin = mean - sd, ymax = mean + sd, size = line_thickness_1),
+                 position = position_dodge(width = 0.7)) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  geom_vline(xintercept = 1:36 + 0.5, size = 0.3, color = "black") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) +
+  ggtitle(paste0("Japanese fly betas of experiments 1 to ", n_experiments)) +
+  scale_size_continuous(range = c(0.5, 2.5))
+
+
+
+betas_level_0_summary_01 %>% 
+  mutate(exp = "All experiments", line_thickness_1 = 2, line_thickness_2 = 1) %>% 
+  bind_rows(betas_level_1_summary_01 %>% 
+              mutate(line_thickness_1 = 0.8, line_thickness_2 = 0.5)) %>% 
+  ggplot(aes(x = variable, color = exp)) +
+  geom_hline(yintercept = 0) +
+  geom_hline(yintercept = 0, size = 0.3) +
+  geom_point(aes(y = mean), position = position_dodge(width = 0.7), size = 0.9) +
+  geom_linerange(aes(ymin = mean - 2*sd, ymax = mean + 2*sd, size = line_thickness_2), 
+                 position = position_dodge(width = 0.7), show.legend = FALSE) +
+  geom_linerange(aes(ymin = mean - sd, ymax = mean + sd, size = line_thickness_1),
+                 position = position_dodge(width = 0.7), show.legend = FALSE) +
+  theme_bw() +
+  xlab("Parameter") +
+  ylab("Value") +
+  geom_vline(xintercept = 1:36 + 0.5, size = 0.3, color = "black") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) +
+  ggtitle(paste0("Japanese fly betas of experiments 1 to ", n_experiments)) +
+  scale_size_continuous(range = c(0.5, 2)) +
+  scale_color_manual(values = c("red", "#381532", "#4b1b42", "#5d2252", "#702963",
+                                "#833074", "#953784", "#a83e95"))
+
+
+
+Sigma_level_0_posterior_median_01 = matrix(
+  as.data.frame(summary(model_stan_01, pars = c("Sigma_level_0"), probs = c(0.5))$summary)$`50%`, 
+  ncol = stan_data$n_var)
 
 
 
