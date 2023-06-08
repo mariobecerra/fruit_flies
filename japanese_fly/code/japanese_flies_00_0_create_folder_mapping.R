c731304ef052c862b49cc263165bbeb815184896
Mario Becerra <mbecerracontreras@gmail.com>
2023-06-08 17:45:42 +0200

    Update counts and design mapping files

--- japanese_fly/code/japanese_flies_00_0_create_folder_mapping.R
+++ japanese_fly/code/japanese_flies_00_0_create_folder_mapping.R
@@ -7,10 +7,11 @@ design_2_mapping = read_csv(here("japanese_fly/out/2nd_i_optimal_design_japanese
 
 design_3_mapping = read_csv(here("japanese_fly/out/3rd_i_optimal_design_japanese_mapping.csv"))
 
+# design_4_mapping = read_csv(here("japanese_fly/out/4th_i_optimal_design_japanese_mapping.csv"))
+
 
 design_mapping = design_1_mapping %>%
-  bind_rows(design_2_mapping)
-bind_rows(design_2_mapping) %>%
+  bind_rows(design_2_mapping) %>% 
   bind_rows(design_3_mapping)
 
 write_csv(design_mapping, "japanese_fly/out/design_mapping_japanese.csv")
